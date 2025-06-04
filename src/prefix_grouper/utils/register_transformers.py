aad9def08ec90f4749a3ff3e4c15033fce49e6f8
johncaged <liuzikang0625@gmail.com>
2025-06-04 12:10:37 +0800

    fix: pyproject error; feat: transformer attention interface.

--- src/prefix_grouper/utils/register_transformers.py
+++ src/prefix_grouper/utils/register_transformers.py
@@ -4,7 +4,7 @@ from ..forward import AttentionForward
 from .typing import Optional
 
 
-def _flash_attention_2_forward(
+def _attention_forward(
     query: torch.Tensor,
     key: torch.Tensor,
     value: torch.Tensor,
@@ -12,14 +12,16 @@ def _flash_attention_2_forward(
     # The following are custom args
     module: torch.nn.Module,
     *args,
+    prefix_grouper_attn_func: str = "flash_attention_2",
     **kwargs,
 ):
     # NOTE: we got q, k, v, attn_mask as the first 4 parameters, while ``flash_attn``
     # requires ``module`` as the first parameter, so we should write an adapter here.
     from transformers.modeling_utils import ALL_ATTENTION_FUNCTIONS
 
-    flash_attn_forward = ALL_ATTENTION_FUNCTIONS["flash_attention_2"]
-    return flash_attn_forward(
+    attn_forward = ALL_ATTENTION_FUNCTIONS[prefix_grouper_attn_func]
+    # NOTE: We do not support returning attention weights for now
+    return attn_forward(
         module,
         query,
         key,
@@ -27,10 +29,10 @@ def _flash_attention_2_forward(
         attention_mask,
         *args,
         **kwargs,
-    )
+    )[0]
 
 
-def _grouped_flash_attention_2(
+def _prefix_grouper_attention_forward(
     module: torch.nn.Module,
     query: torch.Tensor,
     key: torch.Tensor,
@@ -38,13 +40,14 @@ def _grouped_flash_attention_2(
     attention_mask: Optional[torch.Tensor],
     *args,
     prefix_grouper: PrefixGrouper,
+    prefix_grouper_attn_func: str = "flash_attention_2",
     **kwargs,
 ):
     """
     Convert the attention call to ``AttentionForward``
     """
     # NOTE: ``attention_mask`` param is ignored.
-    return AttentionForward(_flash_attention_2_forward)(
+    return AttentionForward(_attention_forward)(
         prefix_grouper,
         query,
         key,
@@ -52,8 +55,9 @@ def _grouped_flash_attention_2(
         # The following are custom parameters
         module,
         *args,
+        prefix_grouper_attn_func=prefix_grouper_attn_func,
         **kwargs,
-    )
+    ), None  # NOTE: We do not support returning attention weights for now.
 
 
 def register_attention():
@@ -62,4 +66,4 @@ def register_attention():
     """
     from transformers.modeling_utils import AttentionInterface
 
-    AttentionInterface.register("grouped_flash_attention_2", _grouped_flash_attention_2)
+    AttentionInterface.register("prefix_grouper_attention", _prefix_grouper_attention_forward)
